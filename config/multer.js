e7c5b7b87122b4846d3ee2fac9fb96b433053a8e
Jayavel <jayavel1520@gmail.com>
2026-01-09 20:47:10 +0530

    feat: Multer configuration.

---
+++ config/multer.js
@@ -0,0 +1,31 @@
+import multer from "multer";
+import path from "path";
+import { v4 as uuidv4 } from "uuid";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    const fileName = `${uuidv4()}${ext}`;
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (!file.mimetype.startsWith("image/")) {
+    const err = new Error("Unsupported file type! Kindly Check it.");
+    err.statusCode = 415;
+    cb(err, false);
+  }
+
+  cb(null, true);
+};
+
+export const upload = multer({
+  storage,
+  fileFilter,
+  limits: {
+    fileSize: 4 * 1024 * 1024,
+  },
+});
