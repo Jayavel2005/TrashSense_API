415e8dda37358599e15046fc4a95344abbf0c7ec
Jayavel <jayavel1520@gmail.com>
2026-01-10 10:47:06 +0530

    feat: Rate limit upgraded.

--- middlewares/ratelimit.middleware.js
+++ middlewares/ratelimit.middleware.js
@@ -3,6 +3,11 @@ import { rateLimit } from "express-rate-limit";
 export const limiter = rateLimit({
   windowMs: 1 * 60 * 1000,
   limit: 10,
-  message: "API rate limit reached.Try again after 1 minutes.",
+  message: {
+    success: false,
+    message: "Too many requests. Please try again after a minute.",
+  },
   statusCode: 429,
+  standardHeaders: true,
+  legacyHeaders: false,
 });
